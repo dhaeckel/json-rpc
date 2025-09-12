de38e28264f48a5a86db0f7bfd267630a3b90bef
Dominik Häckel <dh@dc.ag>
2025-09-12 17:28:37 +0200

    logging and phpdoc

--- src/Server/RequestFactory.php
+++ src/Server/RequestFactory.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Haeckel\JsonRpc\Server;
 
-use Haeckel\JsonRpc\Message;
+use Haeckel\JsonRpc\{Exception, Message};
 
 interface RequestFactory
 {
-    /** @throws \Exception */
+    /**
+     * @throws Exception\JsonParse if json_decode fails
+     * @throws Exception\InvalidRequest if input violates request schema
+     */
     public function newRequest(): Message\Request|Message\Notification|Message\BatchRequest;
 }
