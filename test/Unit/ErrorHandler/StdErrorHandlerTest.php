bd9c66748e32a789042e6668f219ef2c01ee4071
Dominik Häckel <dh@dc.ag>
2025-09-16 19:33:14 +0200

    Add unit test suite and first unit test

---
+++ test/Unit/ErrorHandler/StdErrorHandlerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Haeckel\JsonRpc\Test\Unit\ErrorHandler;
+
+use Haeckel\JsonRpc\{ErrorHandler, Exception, Message, Server};
+use PHPUnit\Framework\Attributes\{CoversClass, Small, UsesClass};
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(ErrorHandler\StdExceptionHandler::class)]
+#[UsesClass(Server\StdEmitter::class)]
+#[UsesClass(Message\Response::class)]
+#[UsesClass(Message\ErrorObject::class)]
+#[UsesClass(Message\PredefinedErrorCode::class)]
+#[UsesClass(Exception\JsonParse::class)]
+#[Small]
+class StdErrorHandlerTest extends TestCase
+{
+    private ErrorHandler\StdExceptionHandler $exceptionHandler;
+
+    public function setUp(): void
+    {
+        $this->exceptionHandler = new ErrorHandler\StdExceptionHandler(new Server\StdEmitter());
+    }
+    public function testWithException(): void
+    {
+        $ex = new \Exception('test error');
+
+        $this->expectOutputString(
+            \json_encode(
+                new Message\Response(
+                    null,
+                    null,
+                    new Message\ErrorObject(Message\PredefinedErrorCode::InternalError),
+                )
+            ),
+        );
+        $this->exceptionHandler->__invoke($ex);
+    }
+
+    public function testWithJsonRpcError(): void
+    {
+        $ex = new Exception\JsonParse();
+
+        $this->expectOutputString(
+            \json_encode(
+                new Message\Response(
+                    null,
+                    null,
+                    new Message\ErrorObject(Message\PredefinedErrorCode::ParseError),
+                ),
+            ),
+        );
+
+        $this->exceptionHandler->__invoke($ex);
+    }
+}
